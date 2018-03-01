d6c64e08ba8f77683e9eaf25b6c4d140bd0e23e0
John Strack <Braveheart22@GMail.com>
2018-03-01 17:07:43 -0500

    Create list fragment

---
+++ app/src/main/java/com/johnstrack/workout/WorkoutListFragment.java
@@ -0,0 +1,28 @@
+package com.johnstrack.workout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkoutListFragment extends ListFragment {
+
+
+    public WorkoutListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
